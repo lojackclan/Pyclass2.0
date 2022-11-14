72d7d4c7a34b958d9d434f843974e56bccd590ae
lojackclan <80369819+lojackclan@users.noreply.github.com>
2022-11-14 16:21:01 -0600

    Add files via upload

---
+++ AVME.py
@@ -0,0 +1,24 @@
+"""Power
+Absolute Value
+Rounding
+F-string formatting
+Capitalization
+Count
+Find
+Extend
+Index
+Insert"""
+
+print(pow(10, 2))
+b = abs(-10)
+print(f"abc {b}")
+text = "yxxyyy"
+text.upper()
+a = [1, 2, 3]
+print(a.count(1))
+a.insert(1,(1,2,3,4))
+a.extend(a)
+text.find(1)
+
+print([x**2 for x in range(10)])
+a.index(3)

4d9965aa4331d98d014a825338dfa8a437876566
lojackclan <80369819+lojackclan@users.noreply.github.com>
2022-12-12 20:57:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
-# This is a sample Python script.
+print("Type NEW to create a new document.")
+print("Type CLOSE to close the document.")
+print("Type SAVE to save.")
+print("Type OPEN to open.")
+print("Type NAME to rename.")
+print("Type EDIT to edit.")
+DocNameList = []
+Line = ""
+LineCount = 0
+CurrentDoc = []
+DocListAndContents = {}
+Writing = 0
+DocName = ""
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def ASK():
+    global LineCount
+    global Line
+    global DocName
+    global Writing
+    StartOption = ""
+    while StartOption != "SAVE" or StartOption != "NEW" or "EDIT" or StartOption != "OPEN" or StartOption != "NAME":
+        StartOption = input("Enter:")
+        if StartOption == "NEW":
+            LineCount = 0
+            Line = ""
+            DocName = input("Document Name:")
+            Writing = 1
+            DocNameList.append(DocName)
+            NEW()
+        if StartOption == "CLOSE":
+            pass
+        if StartOption == "NAME":
+            pass
+        if StartOption == "OPEN":
+            pass
+        if StartOption == "EDIT":
+            EDIT()
+def CLOSE():
+    global CurrentDoc
+    global DocName
+    global Writing
+    CurrentDoc = []
+    DocName = ""
+    Writing = 0
+    StartOption = ""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def NEW():
+    global Writing
+    global LineCount
+    while Writing == 1:
+        Line = input()
+        CurrentDoc.append(Line)
+        LineCount += 1
+        if "CLOSE" in Line:
+            CLOSE()
+
+def EDIT():
+    CurrentDoc[2] = input("Enter:")
+
+ASK()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
